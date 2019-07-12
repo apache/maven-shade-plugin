a7f152068afb43f6e0e772ac3f5f8958ea48aab4
Romain Manni-Bucau <rmannibucau@apache.org>
2019-07-12 20:13:04 +0200

    MSHADE-322 adding properties transformer (+ its microprofile and openwebbeans specific cases)

---
+++ src/main/java/org/apache/maven/plugins/shade/resource/properties/PropertiesTransformer.java
@@ -0,0 +1,195 @@
+package org.apache.maven.plugins.shade.resource.properties;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Properties;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+
+import org.apache.maven.plugins.shade.relocation.Relocator;
+import org.apache.maven.plugins.shade.resource.ResourceTransformer;
+
+/**
+ * Enables to merge a set of properties respecting priority between them.
+ *
+ * @since 3.2.2
+ */
+public class PropertiesTransformer implements ResourceTransformer
+{
+    private String resource;
+    private String alreadyMergedKey;
+    private String ordinalKey;
+    private int defaultOrdinal;
+    private boolean reverseOrder;
+
+    private final List<Properties> properties = new ArrayList<>();
+
+    public PropertiesTransformer()
+    {
+        // no-op
+    }
+
+    protected PropertiesTransformer( final String resource, final String ordinalKey,
+                                     final int defaultOrdinal, final boolean reversed )
+    {
+        this.resource = resource;
+        this.ordinalKey = ordinalKey;
+        this.defaultOrdinal = defaultOrdinal;
+        this.reverseOrder = reversed;
+    }
+
+    @Override
+    public boolean canTransformResource( final String resource )
+    {
+        return Objects.equals( resource, this.resource );
+    }
+
+    @Override
+    public void processResource( final String resource, final InputStream is, final List<Relocator> relocators )
+            throws IOException
+    {
+        final Properties p = new Properties();
+        p.load( is );
+        properties.add( p );
+    }
+
+    @Override
+    public boolean hasTransformedResource()
+    {
+        return !properties.isEmpty();
+    }
+
+    @Override
+    public void modifyOutputStream( final JarOutputStream os ) throws IOException
+    {
+        if ( properties.isEmpty() )
+        {
+            return;
+        }
+
+        final Properties out = mergeProperties( sortProperties() );
+        if ( ordinalKey != null )
+        {
+            out.remove( ordinalKey );
+        }
+        if ( alreadyMergedKey != null )
+        {
+            out.remove( alreadyMergedKey );
+        }
+        os.putNextEntry( new JarEntry( resource ) );
+        out.store( os, "# Merged by maven-shade-plugin (" + getClass().getName() + ")" );
+        os.closeEntry();
+    }
+
+    public void setReverseOrder( final boolean reverseOrder )
+    {
+        this.reverseOrder = reverseOrder;
+    }
+
+    public void setResource( final String resource )
+    {
+        this.resource = resource;
+    }
+
+    public void setOrdinalKey( final String ordinalKey )
+    {
+        this.ordinalKey = ordinalKey;
+    }
+
+    public void setDefaultOrdinal( final int defaultOrdinal )
+    {
+        this.defaultOrdinal = defaultOrdinal;
+    }
+
+    public void setAlreadyMergedKey( final String alreadyMergedKey )
+    {
+        this.alreadyMergedKey = alreadyMergedKey;
+    }
+
+    private List<Properties> sortProperties()
+    {
+        final List<Properties> sortedProperties = new ArrayList<>();
+        boolean foundMaster = false;
+        for ( final Properties current : properties )
+        {
+            if ( alreadyMergedKey != null )
+            {
+                final String master = current.getProperty( alreadyMergedKey );
+                if ( Boolean.parseBoolean( master ) )
+                {
+                    if ( foundMaster )
+                    {
+                        throw new IllegalStateException(
+                                "Ambiguous merged values: " + sortedProperties + ", " + current );
+                    }
+                    foundMaster = true;
+                    sortedProperties.clear();
+                    sortedProperties.add( current );
+                }
+            }
+            if ( !foundMaster )
+            {
+                final int configOrder = getConfigurationOrdinal( current );
+
+                int i;
+                for ( i = 0; i < sortedProperties.size(); i++ )
+                {
+                    int listConfigOrder = getConfigurationOrdinal( sortedProperties.get( i ) );
+                    if ( ( !reverseOrder && listConfigOrder > configOrder )
+                            || ( reverseOrder && listConfigOrder < configOrder ) )
+                    {
+                        break;
+                    }
+                }
+                sortedProperties.add( i, current );
+            }
+        }
+        return sortedProperties;
+    }
+
+    private int getConfigurationOrdinal( final Properties p )
+    {
+        if ( ordinalKey == null )
+        {
+            return defaultOrdinal;
+        }
+        final String configOrderString = p.getProperty( ordinalKey );
+        if ( configOrderString != null && configOrderString.length() > 0 )
+        {
+            return Integer.parseInt( configOrderString );
+        }
+        return defaultOrdinal;
+    }
+
+    private static Properties mergeProperties( final List<Properties> sortedProperties )
+    {
+        final Properties mergedProperties = new Properties();
+        for ( final Properties p : sortedProperties )
+        {
+            mergedProperties.putAll( p );
+        }
+        return mergedProperties;
+    }
+}
---
+++ src/test/java/org/apache/maven/plugins/shade/resource/properties/PropertiesTransformerTest.java
@@ -0,0 +1,113 @@
+package org.apache.maven.plugins.shade.resource.properties;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.apache.maven.plugins.shade.resource.rule.TransformerTesterRule;
+import org.apache.maven.plugins.shade.resource.rule.TransformerTesterRule.Property;
+import org.apache.maven.plugins.shade.resource.rule.TransformerTesterRule.Resource;
+import org.apache.maven.plugins.shade.resource.rule.TransformerTesterRule.TransformerTest;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+
+public class PropertiesTransformerTest
+{
+    @Rule
+    public final TestRule tester = new TransformerTesterRule();
+
+    @Test
+    public void canTransform()
+    {
+        final PropertiesTransformer transformer = new PropertiesTransformer();
+        transformer.setResource("foo/bar/my.properties");
+        assertTrue(transformer.canTransformResource("foo/bar/my.properties"));
+        assertFalse(transformer.canTransformResource("whatever"));
+    }
+
+    @Test
+    @TransformerTest(
+            transformer = PropertiesTransformer.class,
+            configuration = @Property(name = "resource", value = "foo/bar/my.properties"),
+            visited = {
+                    @Resource(path = "foo/bar/my.properties", content = "a=b"),
+                    @Resource(path = "foo/bar/my.properties", content = "c=d"),
+            },
+            expected = @Resource(path = "foo/bar/my.properties", content = "#.*\n#.*\na=b\nc=d\n")
+    )
+    public void mergeWithoutOverlap()
+    {
+    }
+
+    @Test
+    @TransformerTest(
+            transformer = PropertiesTransformer.class,
+            configuration = {
+                    @Property(name = "resource", value = "foo/bar/my.properties"),
+                    @Property(name = "ordinalKey", value = "priority")
+            },
+            visited = {
+                    @Resource(path = "foo/bar/my.properties", content = "a=d\npriority=3"),
+                    @Resource(path = "foo/bar/my.properties", content = "a=b\npriority=1"),
+                    @Resource(path = "foo/bar/my.properties", content = "a=c\npriority=2"),
+            },
+            expected = @Resource(path = "foo/bar/my.properties", content = "#.*\n#.*\na=d\n")
+    )
+    public void mergeWithOverlap()
+    {
+    }
+
+    @Test
+    @TransformerTest(
+            transformer = PropertiesTransformer.class,
+            configuration = {
+                    @Property(name = "resource", value = "foo/bar/my.properties"),
+                    @Property(name = "alreadyMergedKey", value = "complete")
+            },
+            visited = {
+                    @Resource(path = "foo/bar/my.properties", content = "a=b\ncomplete=true"),
+                    @Resource(path = "foo/bar/my.properties", content = "a=c\npriority=2"),
+            },
+            expected = @Resource(path = "foo/bar/my.properties", content = "#.*\n#.*\na=b\n")
+    )
+    public void mergeWithAlreadyMerged()
+    {
+    }
+
+    @Test
+    @TransformerTest(
+            transformer = PropertiesTransformer.class,
+            configuration = {
+                    @Property(name = "resource", value = "foo/bar/my.properties"),
+                    @Property(name = "alreadyMergedKey", value = "complete")
+            },
+            visited = {
+                    @Resource(path = "foo/bar/my.properties", content = "a=b\ncomplete=true"),
+                    @Resource(path = "foo/bar/my.properties", content = "a=c\ncomplete=true"),
+            },
+            expected = {},
+            expectedException = IllegalStateException.class
+    )
+    public void alreadyMergeConflict()
+    {
+    }
+}
