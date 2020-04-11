73377a7fd5b5744de40323a9005b9d086ae8c2f1
Elliotte Rusty Harold <elharo@ibiblio.org>
2020-04-11 09:01:13 -0400

    remove a couple of usages of Guava

--- src/test/java/org/apache/maven/plugins/shade/resource/ServiceResourceTransformerTest.java
+++ src/test/java/org/apache/maven/plugins/shade/resource/ServiceResourceTransformerTest.java
@@ -28,6 +28,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.jar.JarEntry;
@@ -39,18 +40,18 @@ import org.apache.maven.plugins.shade.relocation.Relocator;
 import org.apache.maven.plugins.shade.relocation.SimpleRelocator;
 import org.junit.Test;
 
-import com.google.common.collect.Lists;
-
 /**
  * Test for handling META-INF/service/...
  */
 public class ServiceResourceTransformerTest {
+    
+    private List<Relocator> relocators = new ArrayList<Relocator>();
 
     @Test
     public void relocatedClasses() throws Exception {
         SimpleRelocator relocator =
             new SimpleRelocator( "org.foo", "borg.foo", null, Arrays.asList( "org.foo.exclude.*" ) );
-        List<Relocator> relocators = Lists.<Relocator>newArrayList( relocator );
+        relocators.add( relocator );
 
         String content = "org.foo.Service\norg.foo.exclude.OtherService\n";
         byte[] contentBytes = content.getBytes( StandardCharsets.UTF_8 );
@@ -95,8 +96,8 @@ public class ServiceResourceTransformerTest {
     public void concatanationAppliedMultipleTimes() throws Exception {
         SimpleRelocator relocator =
             new SimpleRelocator( "org.eclipse", "org.eclipse1234", null, null );
-        List<Relocator> relocators = Lists.<Relocator>newArrayList( relocator );
-
+        relocators.add( relocator );
+        
         String content = "org.eclipse.osgi.launch.EquinoxFactory\n";
         byte[] contentBytes = content.getBytes( "UTF-8" );
         InputStream contentStream = new ByteArrayInputStream( contentBytes );
@@ -138,8 +139,8 @@ public class ServiceResourceTransformerTest {
     @Test
     public void concatenation() throws Exception {
         SimpleRelocator relocator = new SimpleRelocator("org.foo", "borg.foo", null, null);
-        List<Relocator> relocators = Lists.<Relocator>newArrayList( relocator );
-
+        relocators.add( relocator );
+        
         String content = "org.foo.Service\n";
         byte[] contentBytes = content.getBytes( StandardCharsets.UTF_8 );
         InputStream contentStream = new ByteArrayInputStream( contentBytes );
