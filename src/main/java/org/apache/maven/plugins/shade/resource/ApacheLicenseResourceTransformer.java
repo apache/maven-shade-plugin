fbe4800f1cf6567408d47335495435e4d8f897d6
John Millner <millnerjohn@gmail.com>
2020-06-07 16:52:00 -0400

    MSHADE-371 - add .md support to transformers

--- src/main/java/org/apache/maven/plugins/shade/resource/ApacheLicenseResourceTransformer.java
+++ src/main/java/org/apache/maven/plugins/shade/resource/ApacheLicenseResourceTransformer.java
@@ -35,11 +35,14 @@ public class ApacheLicenseResourceTransformer
     private static final String LICENSE_PATH = "META-INF/LICENSE";
 
     private static final String LICENSE_TXT_PATH = "META-INF/LICENSE.txt";
+    
+    private static final String LICENSE_MD_PATH = "META-INF/LICENSE.md";
 
     public boolean canTransformResource( String resource )
     {
         return LICENSE_PATH.equalsIgnoreCase( resource )
-            || LICENSE_TXT_PATH.regionMatches( true, 0, resource, 0, LICENSE_TXT_PATH.length() );
+            || LICENSE_TXT_PATH.regionMatches( true, 0, resource, 0, LICENSE_TXT_PATH.length()
+            || LICENSE_MD_PATH.regionMatches( true, 0, resource, 0, LICENSE_MD_PATH.length());
     }
 
     public void processResource( String resource, InputStream is, List<Relocator> relocators, long time )
