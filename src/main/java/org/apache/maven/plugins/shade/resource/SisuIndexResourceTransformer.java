09baabc1ea7eb212405e1385d5e98a6dd931e13d
Tamas Cservenak <tamas@cservenak.net>
2021-05-19 15:06:22 +0200

    [MSHARED-390] Sisu Index transformer
    https://issues.apache.org/jira/browse/MSHADE-390
    
    Adds SISU index transformer to existing ones + IT.

---
+++ src/main/java/org/apache/maven/plugins/shade/resource/SisuIndexResourceTransformer.java
@@ -0,0 +1,162 @@
+package org.apache.maven.plugins.shade.resource;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.maven.plugins.shade.relocation.Relocator;
+
+/**
+ * Resources transformer that relocates classes in {@code META-INF/sisu/javax.inject.Named} and appends resources
+ * into a single resource.
+ *
+ * @since 3.3.0
+ */
+public class SisuIndexResourceTransformer
+    extends AbstractCompatibilityTransformer
+{
+    private static final String SISU_INDEX_PATH = "META-INF/sisu/javax.inject.Named";
+
+    private ServiceStream serviceStream;
+
+    private List<Relocator> relocators;
+
+    private long time = Long.MIN_VALUE;
+
+    @Override
+    public boolean canTransformResource( final String resource )
+    {
+        return resource.equals( SISU_INDEX_PATH );
+    }
+
+    @Override
+    public void processResource( final String resource,
+                                 final InputStream is,
+                                 final List<Relocator> relocators,
+                                 long time ) throws IOException
+    {
+        if ( serviceStream == null )
+        {
+            serviceStream = new ServiceStream();
+        }
+
+        final String content = IOUtils.toString( is, StandardCharsets.UTF_8 );
+        StringReader reader = new StringReader( content );
+        BufferedReader lineReader = new BufferedReader( reader );
+        String line;
+        while ( ( line = lineReader.readLine() ) != null )
+        {
+            String relContent = line;
+            for ( Relocator relocator : relocators )
+            {
+                if ( relocator.canRelocateClass( relContent ) )
+                {
+                    relContent = relocator.applyToSourceContent( relContent );
+                }
+            }
+            serviceStream.append( relContent + "\n" );
+        }
+
+        if ( this.relocators == null )
+        {
+            this.relocators = relocators;
+        }
+
+        if ( time > this.time )
+        {
+            this.time = time;        
+        }
+    }
+
+    @Override
+    public boolean hasTransformedResource()
+    {
+        return serviceStream != null;
+    }
+
+    @Override
+    public void modifyOutputStream( final JarOutputStream jos )
+        throws IOException
+    {
+        ServiceStream data = serviceStream;
+
+        JarEntry jarEntry = new JarEntry( SISU_INDEX_PATH );
+        jarEntry.setTime( time );
+        jos.putNextEntry( jarEntry );
+
+
+        // read the content of service file for candidate classes for relocation.
+        // Specification requires that this file is encoded in UTF-8.
+        Writer writer = new OutputStreamWriter( jos, StandardCharsets.UTF_8 );
+        InputStreamReader streamReader = new InputStreamReader( data.toInputStream() );
+        BufferedReader reader = new BufferedReader( streamReader );
+        String className;
+
+        while ( ( className = reader.readLine() ) != null )
+        {
+            writer.write( className );
+            writer.write( System.lineSeparator() );
+            writer.flush();
+        }
+
+        reader.close();
+        data.reset();
+   }
+
+    static class ServiceStream
+        extends ByteArrayOutputStream
+    {
+
+        ServiceStream()
+        {
+            super( 1024 );
+        }
+
+        public void append( String content )
+            throws IOException
+        {
+            if ( count > 0 && buf[count - 1] != '\n' && buf[count - 1] != '\r' )
+            {
+                write( '\n' );
+            }
+
+            byte[] contentBytes = content.getBytes( StandardCharsets.UTF_8 );
+            this.write( contentBytes );
+        }
+
+        public InputStream toInputStream()
+        {
+            return new ByteArrayInputStream( buf, 0, count );
+        }
+    }
+}
