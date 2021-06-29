9d97ece331d06beabd7a1a69472be1e8c2866178
Niels Basjes <niels@basjes.nl>
2021-06-29 11:10:09 +0200

    Revert code and leave the tests in place

--- src/main/java/org/apache/maven/plugins/shade/mojo/ShadeMojo.java
+++ src/main/java/org/apache/maven/plugins/shade/mojo/ShadeMojo.java
@@ -296,15 +296,6 @@ public class ShadeMojo
     private boolean useDependencyReducedPomInJar;
 
     /**
-     * Hide the shaded/removed dependencies from the rest of the reactor build.
-     * Requires both {@code createDependencyReducedPom} and {@code useDependencyReducedPomInJar} to be {@code true}.
-     *
-     * @since 3.3.0
-     */
-    @Parameter( defaultValue = "false" )
-    private boolean hideReducedDependenciesInReactor;
-
-    /**
      * When true, dependencies are kept in the pom but with scope 'provided'; when false, the dependency is removed.
      */
     @Parameter
@@ -478,25 +469,6 @@ public class ShadeMojo
                     resourceTransformers = new ArrayList<>( resourceTransformers );
                     resourceTransformers.addAll(
                             createPomReplaceTransformers( project, dependencyReducedPomLocation ) );
-
-                    if ( hideReducedDependenciesInReactor )
-                    {
-                        if ( !artifactIds.isEmpty() )
-                        {
-                            getLog().info( "Hiding these shaded dependencies from the rest of the reactor build:" );
-                            getLog().info( artifactIds.toString() );
-                            List<Dependency> dependencies = new ArrayList<>();
-                            for ( Dependency dependency : project.getDependencies() )
-                            {
-                                if ( !artifactIds.contains( getId( dependency ) ) )
-                                {
-                                    dependencies.add( dependency );
-                                }
-                            }
-                            project.setDependencies( dependencies );
-                        }
-                    }
-
                 }
             }
 
