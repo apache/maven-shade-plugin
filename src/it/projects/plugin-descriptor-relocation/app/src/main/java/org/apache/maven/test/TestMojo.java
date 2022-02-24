5898cc05d7df0e8d74cf92d4124102ac4f5dbde3
Guillaume Nodet <gnodet@gmail.com>
2022-02-24 17:00:56 +0100

    Switch a few core plugins to the new api

--- src/it/projects/plugin-descriptor-relocation/app/src/main/java/org/apache/maven/test/TestMojo.java
+++ src/it/projects/plugin-descriptor-relocation/app/src/main/java/org/apache/maven/test/TestMojo.java
@@ -23,7 +23,6 @@ import java.util.List;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -38,7 +37,7 @@ public class TestMojo extends AbstractMojo
     private Entry entry;
     
     public void execute()
-        throws MojoExecutionException, MojoFailureException
+        throws MojoExecutionException
     {
         
     }
