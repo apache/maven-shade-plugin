db62f83bacd1d5086020fc1f5e7d299716211f9c
James Z.M. Gao <gaozhiming@360.cn>
2022-04-05 13:31:05 +0800

    [MSHADE-124] keep original basedir after set d-r-p location to build dir
    From maven 3.2.5, the pom and basedir are seperated
    (https://github.com/apache/maven/commit/c15226f13f6d071d1e926880139d350cc4546b6f),
    and add new api `MavenProject::setPomFile()` to only upodate pom location.
    
    In this pr, the minimal maven version is bumped to 3.2.5, then use
    `setPomFile` to only update pom file location, leaving `${basedir}`
    unchanged.

--- src/main/java/org/apache/maven/plugins/shade/mojo/ShadeMojo.java
+++ src/main/java/org/apache/maven/plugins/shade/mojo/ShadeMojo.java
@@ -267,9 +267,7 @@ public class ShadeMojo
     private boolean createDependencyReducedPom;
 
     /**
-     * Where to put the dependency reduced pom. Note: setting a value for this parameter with a directory other than
-     * ${basedir} will change the value of ${basedir} for all executions that come after the shade execution. This is
-     * often not what you want. This is considered an open issue with this plugin.
+     * Where to put the dependency reduced pom.
      *
      * @since 1.7
      */
@@ -1220,7 +1218,7 @@ public class ShadeMojo
                 modified = updateExcludesInDeps( result.getProject(), dependencies, transitiveDeps );
             }
 
-            project.setFile( dependencyReducedPomLocation );
+            project.setPomFile( dependencyReducedPomLocation );
         }
     }
 
